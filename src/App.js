c6852c0f639da71f4b011284fda039db003a724d
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-02-29 21:59:22 +0900

    コンポーネントについて学んだよ！

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React,{Component} from 'react';
 
-class App extends Component{
-  render(){
-    return(
-    <div>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() =>{console.log("I am clicked")}} />
-     </div>
-    )
-  }
-}
+// class App extends Component{
+//   render(){
+//     return(
+//     <div>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onClick={() =>{console.log("I am clicked")}} />
+//     </div>
+//     )
+//   }
+// }
 
+const App =()=>{
+  return (
+  <div>
+    <Cat/>
+  </div>
+  )
+}
+const Cat =()=>{
+  return <div>meow!</div>
+}
 export default App;
