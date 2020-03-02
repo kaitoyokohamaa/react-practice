45219f498125daeefaa1667a3fe25760e0eb3f57
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-03-02 14:50:21 +0900

    reduxでカウンターアプリを作ったよ！

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -7,5 +7,8 @@ export default (state =initialState,action)=>{
             return {value:state.value +1}
         case DECREMENT:
             return {value:state.value -1}
+        default :
+        return state        
  }
 }
+
